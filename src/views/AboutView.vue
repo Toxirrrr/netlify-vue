3c7312c661263de7dd4859015ad897e1d212d9d1
Toxirrrr <mjuraeva74@gmail.com>
2023-07-14 15:09:08 +0500

    router link added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h1>This is an about page</h1>
+    <router-link to="/">HOME</router-link>
   </div>
 </template>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <h1>This is an home page</h1>
+    <router-link to="/about">ABOUT</router-link>
   </div>
 </template>
 
